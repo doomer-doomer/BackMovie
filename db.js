4d6d7d105895c344521ebd667bbbe1f86a841e22
unknown <tejasgotavade@gmail.com>
2023-04-22 12:51:33 +0530

    added env var4

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool = new Pool({
     user:"postgres",
     password:"tejas94843",
-    host:localhost,//localhost
+    host:"localhost",//localhost
     port:5432,
     database:process.env.DATABASE
 });
