2074ed68a965fa9844ddfd2b81723b325f20f607
unknown <tejasgotavade@gmail.com>
2023-04-22 12:49:59 +0530

    added env var3

--- db.js
+++ db.js
@@ -2,13 +2,12 @@ const Pool = require('pg').Pool;
 //Your .env loaction 
 require('dotenv').config()
 const pool = new Pool({
-    user:process.env.DBUSERNAME,
-    password:process.env.DBPASSWORD,
-    host:process.env.MYHOST,//localhost
-    port:process.env.MYPORT,
+    user:"postgres",
+    password:"tejas94843",
+    host:localhost,//localhost
+    port:5432,
     database:process.env.DATABASE
 });
 
-console.log(""+process.env.DBPASSWORD)
 
 module.exports = pool;
