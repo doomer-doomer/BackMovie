4a4ce6f1ffd0f6a7a9fb3869bdbbeafc7a07c573
unknown <tejasgotavade@gmail.com>
2023-04-22 12:27:28 +0530

    added env var

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool;
 //Your .env loaction 
 require('dotenv').config({path:'C:/Users/Dell/Documents/Tejas/MoviesApp/.env'})
 const pool = new Pool({
-    user:"postgres",
-    password:"tejas94843",
-    host:"127.0.0.1",
-    port:"5432",
-    database:"MovieAppUsers"
+    user:process.env.USERNAME,
+    password:process.env.PASSWORD,
+    host:process.env.MYHOST,//localhost
+    port:process.env.MYPORT,
+    database:process.env.DATABASE
 });
 
 module.exports = pool;
